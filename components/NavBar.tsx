8ccd31544f5e55bf595f0a39bdbebc31fca2d1fb
Parth Sinha <sinhaparth555@gmail.com>
2024-12-14 17:12:16 +0000

    removed menu

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "./ui/button";
 import Link from "next/link";
-import { Album, Menu } from "lucide-react";
+import { Album } from "lucide-react";
 
 export default function NavBar() {
     return (
